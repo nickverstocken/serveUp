b75e7043a059608a93395a15ecac883ce5d7a181
nickverstocken <verstockennick@gmail.com>
2018-03-22 00:54:45 +0100

    profile settings

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
-
+declare var $:any;
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
@@ -9,7 +9,13 @@ import {Router, ActivatedRoute} from '@angular/router';
 export class UserProfileComponent implements OnInit {
   currentId = 0;
   constructor(private router: Router, private route: ActivatedRoute) { }
+  ngAfterViewInit(){
+    //var position = $(".profileEdit").offset();
 
+    $(window).scroll(function () {
+      //var position = $(".profileEdit").offset();
+    });
+  }
   ngOnInit() {
     this.route.params
       .map(params => params['id'])
@@ -18,5 +24,25 @@ export class UserProfileComponent implements OnInit {
         console.log(this.currentId);
       });
   }
+  toggleAccOpened(id){
+    if($('.acBody').is(':visible')){
+      $('.acBody').slideUp(250, 'swing');
+    }
+    if($('#' + id).is(':hidden')){
+      $('#' + id).slideDown(250, 'swing');
+    }
 
+  }
+  editMode(id){
+    $('#' + id + ' .edit').hide();
+    $('#' + id + ' .editting').show();
+    $('#' + id + ' .editable').removeClass('disabled');
+    $('#' + id + ' .editable').attr('readonly', false);
+  }
+  cancelEditMode(id){
+    $('#' + id + ' .edit').show();
+    $('#' + id + ' .editting').hide();
+    $('#' + id + ' .editable').addClass('disabled');
+    $('#' + id + ' .editable').attr('readonly', false);
+  }
 }
